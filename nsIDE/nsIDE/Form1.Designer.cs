f2e27908d22ad5ac4b772ff61a388c8f0800230d
Torin Felton <torinbrody1@gmail.com>
2018-01-30 21:05:56 +0000

    Add files via upload

--- nsIDE/nsIDE/Form1.Designer.cs
+++ nsIDE/nsIDE/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Input = new System.Windows.Forms.RichTextBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -37,29 +36,17 @@
             this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.Output = new System.Windows.Forms.RichTextBox();
             this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.setNodeSCRIPTPathToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.setNodeSCRIPTPathToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.Output = new System.Windows.Forms.RichTextBox();
+            this.Input = new ScintillaNET.Scintilla();
+            this.nodeView = new System.Windows.Forms.TreeView();
             this.menuStrip1.SuspendLayout();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // Input
-            // 
-            this.Input.AcceptsTab = true;
-            this.Input.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.Input.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
-            this.Input.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Input.Location = new System.Drawing.Point(48, 83);
-            this.Input.Name = "Input";
-            this.Input.Size = new System.Drawing.Size(2017, 827);
-            this.Input.TabIndex = 0;
-            this.Input.Text = "";
             // 
             // menuStrip1
             // 
@@ -71,7 +58,8 @@
             this.settingsToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(2128, 33);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 1, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(1283, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -83,71 +71,48 @@
             this.openToolStripMenuItem,
             this.saveAsToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(50, 29);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 22);
             this.fileToolStripMenuItem.Text = "File";
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(218, 30);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
             this.saveToolStripMenuItem.Text = "Save (Ctrl+S)";
             this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
             // newToolStripMenuItem
             // 
             this.newToolStripMenuItem.Name = "newToolStripMenuItem";
-            this.newToolStripMenuItem.Size = new System.Drawing.Size(218, 30);
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
             this.newToolStripMenuItem.Text = "New (Ctrl + N)";
             this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
             // 
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(218, 30);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
             this.openToolStripMenuItem.Text = "Open (Ctrl + O)";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
             // saveAsToolStripMenuItem
             // 
             this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
-            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(218, 30);
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
             this.saveAsToolStripMenuItem.Text = "Save As";
             this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
             // 
             // editToolStripMenuItem
             // 
             this.editToolStripMenuItem.Name = "editToolStripMenuItem";
-            this.editToolStripMenuItem.Size = new System.Drawing.Size(54, 29);
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 22);
             this.editToolStripMenuItem.Text = "Edit";
             // 
             // viewToolStripMenuItem
             // 
             this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
-            this.viewToolStripMenuItem.Size = new System.Drawing.Size(61, 29);
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 22);
             this.viewToolStripMenuItem.Text = "View";
-            // 
-            // toolStrip1
-            // 
-            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripButton1});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 33);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(2128, 31);
-            this.toolStrip1.TabIndex = 2;
-            this.toolStrip1.Text = "toolStrip1";
-            // 
-            // Output
-            // 
-            this.Output.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.Output.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
-            this.Output.Location = new System.Drawing.Point(48, 925);
-            this.Output.Name = "Output";
-            this.Output.ReadOnly = true;
-            this.Output.Size = new System.Drawing.Size(2017, 328);
-            this.Output.TabIndex = 3;
-            this.Output.Text = "";
             // 
             // settingsToolStripMenuItem
             // 
@@ -155,15 +120,33 @@
             this.toolStripMenuItem1,
             this.setNodeSCRIPTPathToolStripMenuItem});
             this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
-            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(88, 29);
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 22);
             this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(198, 22);
+            this.toolStripMenuItem1.Text = "Show nodeSCRIPT Path";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
             // 
             // setNodeSCRIPTPathToolStripMenuItem
             // 
             this.setNodeSCRIPTPathToolStripMenuItem.Name = "setNodeSCRIPTPathToolStripMenuItem";
-            this.setNodeSCRIPTPathToolStripMenuItem.Size = new System.Drawing.Size(281, 30);
+            this.setNodeSCRIPTPathToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
             this.setNodeSCRIPTPathToolStripMenuItem.Text = "Set nodeSCRIPT Path";
             this.setNodeSCRIPTPathToolStripMenuItem.Click += new System.EventHandler(this.setNodeSCRIPTPathToolStripMenuItem_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1283, 31);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
             // 
             // toolStripButton1
             // 
@@ -175,24 +158,60 @@
             this.toolStripButton1.Text = "Run";
             this.toolStripButton1.Click += new System.EventHandler(this.Run_CLick);
             // 
-            // toolStripMenuItem1
+            // Output
             // 
-            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(281, 30);
-            this.toolStripMenuItem1.Text = "Show nodeSCRIPT Path";
-            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            this.Output.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Output.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.Output.Location = new System.Drawing.Point(0, 601);
+            this.Output.Margin = new System.Windows.Forms.Padding(2);
+            this.Output.Name = "Output";
+            this.Output.ReadOnly = true;
+            this.Output.Size = new System.Drawing.Size(1378, 215);
+            this.Output.TabIndex = 3;
+            this.Output.Text = "";
+            // 
+            // Input
+            // 
+            this.Input.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Input.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Input.CaretForeColor = System.Drawing.Color.White;
+            this.Input.Lexer = ScintillaNET.Lexer.Null;
+            this.Input.Location = new System.Drawing.Point(12, 58);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(1074, 537);
+            this.Input.TabIndex = 4;
+            this.Input.UseTabs = true;
+            this.Input.CharAdded += new System.EventHandler<ScintillaNET.CharAddedEventArgs>(this.Input_CharAdded);
+            this.Input.TextChanged += new System.EventHandler(this.Input_TextChanged);
+            // 
+            // nodeView
+            // 
+            this.nodeView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.nodeView.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.nodeView.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nodeView.Location = new System.Drawing.Point(1092, 58);
+            this.nodeView.Name = "nodeView";
+            this.nodeView.Size = new System.Drawing.Size(191, 538);
+            this.nodeView.TabIndex = 5;
             // 
             // nsIDE
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
-            this.ClientSize = new System.Drawing.Size(2128, 1293);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(1283, 690);
+            this.Controls.Add(this.nodeView);
+            this.Controls.Add(this.Input);
             this.Controls.Add(this.Output);
             this.Controls.Add(this.toolStrip1);
-            this.Controls.Add(this.Input);
             this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "nsIDE";
             this.Text = "nsIDE";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -219,10 +238,11 @@
         private System.Windows.Forms.ToolStripButton toolStripButton1;
         private System.Windows.Forms.RichTextBox Output;
         private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
-        private System.Windows.Forms.RichTextBox Input;
         private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem setNodeSCRIPTPathToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private ScintillaNET.Scintilla Input;
+        private System.Windows.Forms.TreeView nodeView;
     }
 }
 
--- nsIDE/nsIDE/Form1.cs
+++ nsIDE/nsIDE/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ScintillaNET;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -22,6 +23,82 @@ namespace nsIDE
         {
             InitializeComponent();
             this.Text += " - " + fileName;
+
+
+            Input.StyleResetDefault();
+            Input.Styles[Style.Default].BackColor = Color.FromArgb(30, 30, 30);
+            Input.Styles[Style.Default].ForeColor = Color.White;
+            Input.Styles[Style.Default].Size = 14;
+            Input.ScrollWidth = 1;
+            Input.ScrollWidthTracking = true;
+            Input.StyleClearAll();
+            Input.Styles[Style.LineNumber].BackColor = Color.FromArgb(30, 30, 30);
+            Input.Styles[Style.LineNumber].ForeColor = Color.FromArgb(40, 145, 175);
+
+
+
+            Input.Lexer = ScintillaNET.Lexer.Cpp; // Doesn't matter what group it's in, cpp is empty
+
+
+            //
+            // Comment highlighting
+            //
+            Input.Margins[0].Width = 16;
+            Input.Styles[Style.Cpp.CommentLine].Font = "Consolas";
+            Input.Styles[Style.Cpp.CommentLine].ForeColor = Color.FromArgb(0, 128, 0); // Green
+
+            //
+            // string highlighting
+            //
+            Input.Styles[Style.Cpp.String].Font = "Consolas";
+            Input.Styles[Style.Cpp.String].ForeColor = Color.FromArgb(163, 21, 21); // Red
+            Input.Styles[Style.Cpp.Default].ForeColor = Color.Silver;
+            Input.Styles[Style.Cpp.Comment].ForeColor = Color.FromArgb(0, 128, 0); // Green
+            Input.Styles[Style.Cpp.CommentLine].ForeColor = Color.FromArgb(0, 128, 0); // Green
+            Input.Styles[Style.Cpp.CommentLineDoc].ForeColor = Color.FromArgb(128, 128, 128); // Gray
+            Input.Styles[Style.Cpp.Number].ForeColor = Color.Olive;
+            Input.Styles[Style.Cpp.Word].ForeColor = Color.FromArgb(78, 171, 110); // group 0
+            Input.Styles[Style.Cpp.Word2].ForeColor = Color.FromArgb(58, 136, 212);
+            Input.Styles[Style.Cpp.String].ForeColor = Color.FromArgb(163, 21, 21); // Red
+            Input.Styles[Style.Cpp.Character].ForeColor = Color.FromArgb(163, 21, 21); // Red
+            Input.Styles[Style.Cpp.Verbatim].ForeColor = Color.FromArgb(163, 21, 21); // Red
+            Input.Styles[Style.Cpp.StringEol].BackColor = Color.Pink;
+            Input.Styles[Style.Cpp.Operator].ForeColor = Color.Purple;
+            Input.Styles[Style.Cpp.Preprocessor].ForeColor = Color.Maroon;
+            Input.SetKeywords(0, "main node while for foreach");
+            Input.SetKeywords(1, "var obj object variable if");
+
+            Input.SetProperty("fold", "1");
+            Input.SetProperty("fold.compact", "1");
+
+            // Configure a margin to display folding symbols
+            Input.Margins[2].Type = MarginType.Symbol;
+            Input.Margins[2].Mask = Marker.MaskFolders;
+            Input.Margins[2].Sensitive = true;
+            Input.Margins[2].Width = 20;
+            Input.SetFoldMarginColor(true, Color.FromArgb(30, 30, 30));
+            Input.SetFoldMarginHighlightColor(true, Color.FromArgb(30, 30, 30));
+
+
+            // Set colors for all folding markers
+            for (int i = 25; i <= 31; i++)
+            {
+                Input.Markers[i].SetForeColor(Color.FromArgb(30, 30, 30));
+                Input.Markers[i].SetBackColor(Color.FromArgb(189, 192, 198));
+            }
+
+            // Configure folding markers with respective symbols
+            Input.Markers[Marker.Folder].Symbol = MarkerSymbol.BoxPlus;
+            Input.Markers[Marker.FolderOpen].Symbol = MarkerSymbol.BoxMinus;
+            Input.Markers[Marker.FolderEnd].Symbol = MarkerSymbol.BoxPlusConnected;
+            Input.Markers[Marker.FolderMidTail].Symbol = MarkerSymbol.TCorner;
+            Input.Markers[Marker.FolderOpenMid].Symbol = MarkerSymbol.BoxMinusConnected;
+            Input.Markers[Marker.FolderSub].Symbol = MarkerSymbol.VLine;
+            Input.Markers[Marker.FolderTail].Symbol = MarkerSymbol.LCorner;
+
+            // Enable automatic folding
+            Input.AutomaticFold = (AutomaticFold.Show | AutomaticFold.Click | AutomaticFold.Change);
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -208,6 +285,37 @@ namespace nsIDE
         private void toolStripMenuItem1_Click(object sender, EventArgs e)
         {
             MessageBox.Show(Properties.Settings.Default.Path);
+        }
+
+        private int maxLineNumberCharLength;
+        private void Input_TextChanged(object sender, EventArgs e)
+        {
+            // Did the number of characters in the line number display change?
+            // i.e. nnn VS nn, or nnnn VS nn, etc...
+            var maxLineNumberCharLength = Input.Lines.Count.ToString().Length;
+            if (maxLineNumberCharLength == this.maxLineNumberCharLength)
+                return;
+
+            // Calculate the width required to display the last line number
+            // and include some padding for good measure.
+            const int padding = 2;
+            Input.Margins[0].Width = Input.TextWidth(Style.LineNumber, new string('9', maxLineNumberCharLength + 1)) + padding;
+            this.maxLineNumberCharLength = maxLineNumberCharLength;
+        }
+
+        private void Input_CharAdded(object sender, CharAddedEventArgs e)
+        {
+            // Find the word start
+            var currentPos = Input.CurrentPosition;
+            var wordStartPos = Input.WordStartPosition(currentPos, true);
+
+            // Display the autocompletion list
+            var lenEntered = currentPos - wordStartPos;
+            if (lenEntered > 0)
+            {
+                if (!Input.AutoCActive)
+                    Input.AutoCShow(lenEntered, "main node var obj if while for foreach");
+            }
         }
     }
 }
